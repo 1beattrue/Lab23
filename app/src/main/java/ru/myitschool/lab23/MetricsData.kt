d1b64e4300bf3995053d9f32c88f6e93b97edf2e
Vitaly <onebeattrue@gmail.com>
2023-11-19 02:18:50 +0300

    Поправил репозиторий

--- app/src/main/java/ru/myitschool/lab23/MetricsData.kt
+++ app/src/main/java/ru/myitschool/lab23/MetricsData.kt
@@ -3,53 +3,49 @@ package ru.myitschool.lab23
 import kotlin.math.pow
 
 class MetricsData {
-    private val coefficients = listOf(
-        39.3701,
-        1.09361,
-        3.28084,
-        0.000_621_371,
-        10.0.pow(24),
-        10.0.pow(21),
-        10.0.pow(18),
-        10.0.pow(15),
-        10.0.pow(12),
-        10.0.pow(9),
-        10.0.pow(6),
-        10.0.pow(3),
-        10.0.pow(2),
-        10.0.pow(1),
-        1.0,
-        10.0.pow(-1),
-        10.0.pow(-2),
-        10.0.pow(-3),
-        10.0.pow(-6),
-        10.0.pow(-9),
-        10.0.pow(-12),
-        10.0.pow(-15),
-        10.0.pow(-18),
-        10.0.pow(-21),
-        10.0.pow(-24),
-    )
-
-    private val metrics = MutableList(25) { START_VALUE }
+    private val metrics = MutableList(COEFFICIENTS.size) { START_VALUE }
 
     fun recalculate(index: Int, value: String): List<Double> {
         val valueDouble = value.toDoubleOrNull() ?: 0.0
-
-        val metres = convertToMetres(index, valueDouble)
-
+        val valueMetres = convertToMetres(index, valueDouble)
         for (i in metrics.indices) {
-            metrics[i] = metres / coefficients[i]
+            metrics[i] = valueMetres / COEFFICIENTS[i]
         }
-
         return metrics.toList()
     }
 
     private fun convertToMetres(index: Int, value: Double): Double {
-        return value * coefficients[index]
+        return value * COEFFICIENTS[index]
     }
 
     companion object {
         private const val START_VALUE = 0.0
+        private val COEFFICIENTS = listOf(
+            39.3701,
+            1.09361,
+            3.28084,
+            0.000_621_371,
+            10.0.pow(24),
+            10.0.pow(21),
+            10.0.pow(18),
+            10.0.pow(15),
+            10.0.pow(12),
+            10.0.pow(9),
+            10.0.pow(6),
+            10.0.pow(3),
+            10.0.pow(2),
+            10.0.pow(1),
+            1.0,
+            10.0.pow(-1),
+            10.0.pow(-2),
+            10.0.pow(-3),
+            10.0.pow(-6),
+            10.0.pow(-9),
+            10.0.pow(-12),
+            10.0.pow(-15),
+            10.0.pow(-18),
+            10.0.pow(-21),
+            10.0.pow(-24),
+        )
     }
 }
