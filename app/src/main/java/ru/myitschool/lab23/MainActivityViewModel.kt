b0c4ea9e441d4fd1276d0104bb4d8f37ae4e05c4
Vitaly <onebeattrue@gmail.com>
2023-11-19 20:25:22 +0300

    Поправил доступ к flow из ViewModel

--- app/src/main/java/ru/myitschool/lab23/MainActivityViewModel.kt
+++ app/src/main/java/ru/myitschool/lab23/MainActivityViewModel.kt
@@ -4,14 +4,14 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 class MainActivityViewModel : ViewModel() {
     private val repository = MetricsData()
 
     private val _metrics = MutableSharedFlow<List<Double>>(1)
-    val metrics: Flow<List<Double>>
-        get() = _metrics
+    val metrics = _metrics.asSharedFlow()
 
     fun recalculate(index: Int, value: String) {
         viewModelScope.launch {
